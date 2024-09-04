fb946aa93416984987b0f4e029f4eeeb93a80808
alexic2 <alex.cano@duocuc.cl>
2024-09-04 13:58:29 -0400

    Página pasajero

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,11 @@ const routes: Routes = [
   {
     path: 'inicio',
     loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
+  },  {
+    path: 'pasajero',
+    loadChildren: () => import('./pasajero/pasajero.module').then( m => m.PasajeroPageModule)
   },
+
 ];
 
 @NgModule({
