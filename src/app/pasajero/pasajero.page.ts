e66d2104f8a5f62dfa833254a487ca98c522d038
alexic2 <alex.cano@duocuc.cl>
2024-09-05 01:25:29 -0400

    Edit. pasajero

--- src/app/pasajero/pasajero.page.ts
+++ src/app/pasajero/pasajero.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-pasajero',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pasajero.page.scss'],
 })
 export class PasajeroPage implements OnInit {
+  alertSrv = inject(AlertController);
+  navController = inject(NavController);
 
   constructor() { }
 
   ngOnInit() {
   }
+  async mostrarAlerta() {
+    const alert = await this.alertSrv.create({
+      header: 'Viaje Solicitado',
+      message: 'Ya se ha solicitado el viaje.',
+      buttons: [
+        {
+          text: 'ok'
+        }
 
+      ]
+      
+    });
+  this.navController.navigateRoot('home')
+    await alert.present();
+  }
+  
+  async volver(){
+    this.navController.pop();
+  }
 }
